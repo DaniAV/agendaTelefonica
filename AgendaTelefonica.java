a29a03f4b684b9fd72e849722d68e8cb1413b3e2
Daniel <daniel.alvarez.vaquero@gmail.com>
2015-02-25 11:57:41 +0100

    Añadidos metodos enterNumber y lookupNumber

---
+++ AgendaTelefonica.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+/**
+ * Write a description of class AgendaTelefonica here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class AgendaTelefonica
+{
+    // instance variables - replace the example below with your own
+    private HashMap<String, String> agenda; 
+
+    /**
+     * Constructor for objects of class AgendaTelefonica
+     */
+    public AgendaTelefonica()
+    {
+        agenda = new HashMap<>();
+      
+    }
+
+    /**
+     * Metodo que permite introducir un contacto a la agenda
+     */
+    public void enterNumber(String name, String number)
+    {
+        agenda.put(name, number);
+    }
+    
+    /**
+     * Muestra el número asociado al nombre dado
+     */
+    public void lookupNumber(String name)
+    {
+        System.out.println(name + " : " + agenda.get(name));
+    }
+}
